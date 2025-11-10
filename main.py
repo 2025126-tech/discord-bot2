e84ca9c80e51bba14d6338fdcfbaca3181d2cf92
2025126-tech <tako-2025126@edu-g.gsn.ed.jp>
2025-11-11 00:40:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 import discord
 from discord.ext import commands
 
 # ボットのトークンはあとで差し替える
-TOKEN = "TOKEN"
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
